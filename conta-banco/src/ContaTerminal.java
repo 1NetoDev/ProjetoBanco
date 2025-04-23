59825d96c7af3d43d514c82aa5940f7fdd81892a
1NetoDev <anderaes@gmail.com>
2025-04-23 01:47:56 -0300

    Versao Corrigida Atualizada

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -18,15 +18,18 @@ public class ContaTerminal {
         //Definição de variáveis para armazenar informações posteriormente
         double transferencia = 0;
         int opcao = 0;
-        String tipoConta = "Conta Corrente"; // Inicializa a variável tipoConta com um valor padrão
+        cliente1.tipoConta = "Conta Corrente"; // Inicializa a variável tipoConta com um valor padrão
 
         //Obtenção de dados do cliente
-        System.out.println("Por favor, digite o número da Agência: ");
-        cliente1.agencia = scanner.nextLine();
-        System.out.println("Por favor, digite o número da Conta: ");
-        cliente1.numeroConta = scanner.nextInt();
         System.out.println("Por favor, digite o seu nome: ");
         cliente1.nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        cliente1.agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        cliente1.numeroConta = scanner.nextInt();
+
         System.out.println("Por favor, digite o saldo: ");
         cliente1.saldo = scanner.nextDouble();
 
@@ -35,17 +38,17 @@ public class ContaTerminal {
         System.out.println("Digite 1 para corrente ou 2 para poupança: ");
         opcao = scanner.nextInt();
         if (opcao == 1){
-            tipoConta = "Conta Corrente";
+            cliente1.tipoConta = "Conta Corrente";
             System.out.println("Você escolheu conta corrente.");
         }
         else if (opcao == 2){
-            tipoConta = "Conta Poupança";
+            cliente1.tipoConta = "Conta Poupança";
             System.out.println("Você escolheu conta poupança.");
         }
         else {
             System.out.println("Opção inválida, conta corrente será criada por padrão.");
         }
-        System.out.println("Olá " + cliente1.nomeCliente + ", Seja muito bem vindo, sua agência é " + cliente1.agencia + ", conta " + cliente1.numeroConta +cliente1.tipoConta + " e seu saldo R$" + cliente1.saldo + " já está disponível para saque.");
+        System.out.println("Olá " + cliente1.nomeCliente + ", Seja muito bem vindo, sua agência é " + cliente1.agencia + ", " + cliente1.tipoConta + " Número: " + cliente1.numeroConta + " e seu saldo R$" + cliente1.saldo + " já está disponível para saque.");
         System.out.println("Confira nosso menu de opções: \n");
 
         //Menu Opções
