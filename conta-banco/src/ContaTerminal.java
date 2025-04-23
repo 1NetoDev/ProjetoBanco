b1a774f1805aede87e14b23b76c9970a11d45b05
1NetoDev <anderaes@gmail.com>
2025-04-23 00:44:46 -0300

    aTUAlizaçao com bug

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /* Afazeres:
+        Conhecer e Importar a classe scanner
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+        Exibir a mensagem conta criada com sucesso
+        */
+
+        //Instancia a classe Cliente para criar um novo cliente
+        Scanner scanner = new Scanner(System.in);
+        Cliente cliente1 = new Cliente();
+
+        System.out.println("Bem-vindo ao DIO-Bank!😁\n");
+
+        //Definição de variáveis para armazenar informações posteriormente
+        double transferencia = 0;
+        int opcao = 0;
+        String tipoConta = "Conta Corrente"; // Inicializa a variável tipoConta com um valor padrão
+
+        //Obtenção de dados do cliente
+        System.out.println("Por favor, digite o número da Agência: ");
+        cliente1.agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da Conta: ");
+        cliente1.numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite o seu nome: ");
+        cliente1.nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo: ");
+        cliente1.saldo = scanner.nextDouble();
+
+        //definição tipo de conta
+        System.out.println("Você prefere que seja conta corrente ou conta poupança?: ");
+        System.out.println("Digite 1 para corrente ou 2 para poupança: ");
+        opcao = scanner.nextInt();
+        if (opcao == 1){
+            tipoConta = "Conta Corrente";
+            System.out.println("Você escolheu conta corrente.");
+        }
+        else if (opcao == 2){
+            tipoConta = "Conta Poupança";
+            System.out.println("Você escolheu conta poupança.");
+        }
+        else {
+            System.out.println("Opção inválida, conta corrente será criada por padrão.");
+        }
+        System.out.println("Olá " + cliente1.nomeCliente + ", Seja muito bem vindo, sua agência é " + cliente1.agencia + ", conta " + cliente1.numeroConta +cliente1.tipoConta + " e seu saldo R$" + cliente1.saldo + " já está disponível para saque.");
+        System.out.println("Confira nosso menu de opções: \n");
+
+        //Menu Opções
+        cliente1.chamaMenu();
+
     }
 }
